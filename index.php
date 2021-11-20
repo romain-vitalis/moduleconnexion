f623a95ddf8a59d51901f79d1771c2a2b184ca02
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-20 19:57:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
 <nav>
         <div>
-        <img src="https://www.makrea.com/1441-large_default/sticker-logo-olympique-de-marseille.jpg" alt="Logo OM" width="145px" height="145px"></a>
+        <img src="https://www.makrea.com/1441-large_default/sticker-logo-olympique-de-marseille.jpg" alt="Logo OM" width="145px" height="145px">
         </div>
 <ul class="Menu">
     <li>Accueil</li>
@@ -28,17 +28,20 @@
         <section>
             <div>
                 <h1>Présentation de notre club</h1><br>
-              Le club a été crée en 1899<br>
+              <p>Le club a été crée en 1899<br>
               Le palmarès de l'OM :
               Champion (11 titres – 9 comptés) : 1929*- 1937 – 1948 – 1971 – 1972 – 1989 – 1990 – 1991 – 1992 – 1993* – 2010<br>
               Coupe de France
               Vainqueur (10 titres) : 1924 – 1926 – 1927 – 1935 – 1938 – 1943 – 1969 – 1972 – 1976 – 1989<br>
               Coupe de la Ligue
               Vainqueur (3 titres) : 2010, 2011, 2012
-            
+              Trophée des Champions 
+              Vainqueur : 2010, 2011
+              Ligue des champions 
+              Vainqueur : 1993</p>
             <h2>OL-OM les stats d'avant-match</h2>
             <br>
-            L'OM compte 6 clean sheets en Ligue 1 Uber Eats cette saison, plus que toute autre équipe.<br>
+            <p>L'OM compte 6 clean sheets en Ligue 1 Uber Eats cette saison, plus que toute autre équipe.<br>
             L’OM n’a d’ailleurs pas encaissé de but lors de 3 de ses 4 dernières rencontres dans l’élite. <br>
             Dimitri Payet est impliqué sur 13 buts face à l'OL en Ligue 1 Uber Eats (4 buts, 9 passes décisives), son meilleur total face à une équipe actuelle de l’élite.<br>
 </main>
