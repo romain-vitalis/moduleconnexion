0fd26fae39441d09d7c29de8f5b7e01b35e98594
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-20 19:44:58 +0100

    ajout accueil site

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="moduleconnexion.css">
+    <title>Mon site</title>
+</head>
+<body>
+<nav>
+        <div>
+        <img src="https://www.makrea.com/1441-large_default/sticker-logo-olympique-de-marseille.jpg" alt="Logo OM" width="145px" height="145px"></a>
+        </div>
+        <ul class="Menu">
+    <li>Accueil</li>
+
+    <li>Boutique</li>
+
+    <li>Billeterie</li>
+
+    <li><a href="inscription.php">Inscription</a></li>
+
+    <li><a href="connexion.php">Connexion</a></li>
+    
+    <li><a href="profil.php">Profil</a></li>
+</ul>
+    </nav> 
+    <header> 
+        <img src="https://www.leparisien.fr/resizer/e9VQt5CzgXxTsxJ4vV6jADkRuqk=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/GQKKHDS7U5BAHNKBS37LFQHH6A.jpg" width="1500px"  height="400px">
+    </header>
+<main>
+        <section>
+            <div>
+                <h1>Présentation de notre club</h1>
+                </br>
+              Le club a été crée en 1899<br>
+              Le palmarès de l'OM :
+              Champion (11 titres – 9 comptés) : 1929*- 1937 – 1948 – 1971 – 1972 – 1989 – 1990 – 1991 – 1992 – 1993* – 2010<br>
+              Coupe de France
+              Vainqueur (10 titres) : 1924 – 1926 – 1927 – 1935 – 1938 – 1943 – 1969 – 1972 – 1976 – 1989<br>
+              Coupe de la Ligue
+              Vainqueur (3 titres) : 2010, 2011, 2012
+            </p>
+            <h2>OL-OM les stats d'avant-match</h2>
+            <br>
+            L'OM compte 6 clean sheets en Ligue 1 Uber Eats cette saison, plus que toute autre équipe.<br>
+            L’OM n’a d’ailleurs pas encaissé de but lors de 3 de ses 4 dernières rencontres dans l’élite. <br>
+            Dimitri Payet est impliqué sur 13 buts face à l'OL en Ligue 1 Uber Eats (4 buts, 9 passes décisives), son meilleur total face à une équipe actuelle de l’élite.<br>
+</main>
+        <section class="fan">
+            <a>Rejoins le fan club de l'OM en cliquant sur l'onglet Inscription</a> 
+                <br>
+            <a>Sinon connectez-vous sur l'onglet Connexion</a> 
+            </p>
+</body>
+</html>
