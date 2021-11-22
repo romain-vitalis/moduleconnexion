759e7eb7b486c04f267f046a5eafa5fc52a5bea9
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-22 15:27:27 +0100

    modification

--- connexion.php
+++ connexion.php
@@ -29,8 +29,8 @@ if(isset($_POST['login']) && isset($_POST['mdp'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="moduleconnexions.css">
-    <title>$connexion</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Connexion</title>
 </head>
 <body>
 <div class ="connexion">
