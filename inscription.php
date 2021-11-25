867c3307b60acc356a32b79e8208a220b52d0a02
romain-vitalis <romain.vitalis@laplateforme.iO>
2021-11-25 13:06:36 +0100

    style ajouter

--- inscription.php
+++ inscription.php
@@ -21,7 +21,7 @@ if (isset($_POST['env']))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="moduleconnexions.css">
+    <link rel="stylesheet" href="style.css">
     <title>Inscription</title>
 </head>
 <body>
